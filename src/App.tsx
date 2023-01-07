494bd8ce0d1b47a29f16ab17a741782ac45ab185
LexaFollowYourDream <aleksejkundenko10@gmail.com>
2023-01-07 18:01:45 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React, {useState} from 'react';
+import './App.css';
+
+
+function App() {
+
+    let [a, setA] = useState(1)
+
+    const onClickHandler = () => {
+        setA(++a)
+        console.log(a)
+    }
+    const onClickHandlerZero = () => {
+        setA(a = 0)
+    }
+
+    return (
+        <div className="App">
+            <h1>{a}</h1>
+            <button onClick={onClickHandler}>number +</button>
+            <button onClick={onClickHandlerZero}>number 0</button>
+        </div>
+
+    )
+}
+
+export default App;
